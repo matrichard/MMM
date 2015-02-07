66ead02006af731f67151248f1e9055e2758ed62
matrichard <mathieu.richard5@gmail.com>
2015-02-07 14:29:02 -0500

    basic member structure

---
+++ MeetupMeetingManagement/Scripts/application/app.js
@@ -0,0 +1,22 @@
+﻿(function (angular) {
+	"use strict";
+
+	angular.module('myApp', []);
+})(window.angular);
+
+//MainCtrl
+(function (angular) {
+	"use strict";
+
+	angular.module("myApp").controller("MainCtrl", function() {
+			this.greeting = "hola from mainCtrl";
+		});
+})(window.angular);
+
+//Member Service
+(function (angular) {
+	var Member = 
+	angular.module('myApp').service('memberSrv', function() {
+		
+	});
+})(window.angular);
