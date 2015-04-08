a6ef8824e3333ea209e575b3d2b24b65886bd25f
matrichard <mathieu.richard5@gmail.com>
2015-04-07 23:13:38 -0400

    localstorage expiration time

--- MeetupMeetingManagement/Scripts/application/app.js
+++ MeetupMeetingManagement/Scripts/application/app.js
@@ -71,19 +71,47 @@
 //Events Service
 (function (angular) {
 	angular.module('myApp').service('eventSrv', function ($http, $q, $localStorage) {
-		function loadFromSite() {
+
+		function saveToStorage(key, data, expiration) {
+			if (expiration === undefined) {
+				expiration = 1 * 60 * 1000;
+			}
+
+			var now = new Date();
+			var storedData = {
+				timestamp: now.getTime(),
+				data: data,
+				expiration: new Date(now.getTime() + expiration).getTime()
+		}
+			
+			$localStorage[key] = storedData;
+		}
+
+		function loadFromStorage(key) {
+			var storedData = $localStorage[key];
+			
+			if (!storedData || !storedData.expiration || storedData.expiration < Date.now()) {
+				delete $localStorage[key];
+				return undefined;
+			}
+
+			return storedData.data;
+		}
+
+		var loadFromSite = function() {
 			var deferred = $q.defer();
 			$http.get("/api/events").then(function (result) {
-				$localStorage.events = result;
+				saveToStorage("events", result.data);
 				deferred.resolve(result);
 			}).catch(function(error) {
 				deferred.reject(error);
 			});
+
 			return deferred.promise;
 		}
 
-		function loadEvents() {
-			var events = $localStorage.events;
+		var loadEvents = function() {
+			var events = loadFromStorage("events");
 			if (events) {
 				return $q.when(events);
 			} else {
